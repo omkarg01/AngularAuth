e11328bc208d588230dfede9d422e0fcc22eead6
omkarg01 <omkargujja01@gmail.com>
2024-08-10 23:34:36 +0530

    unused compo removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { UserExistComponent } from './user-exist/user-exist.component';
-import { CounterComponent } from './counter/counter.component';
 import { LoginSuccessComponent } from './login-success/login-success.component';
 import { SignupDetailComponent } from './signup-detail/signup-detail.component';
 import { SignupSuccessComponent } from './signup-success/signup-success.component';
-import { TryTailwindComponent } from './try-tailwind/try-tailwind.component';
 
 export const routes: Routes = [
   {
@@ -26,19 +24,11 @@ export const routes: Routes = [
     component: LoginSuccessComponent,
   },
   {
-    path: 'counter',
-    component: CounterComponent,
-  },
-  {
     path: 'signup-details',
     component: SignupDetailComponent,
   },
   {
     path: 'signup-success',
     component: SignupSuccessComponent,
-  },
-  {
-    path: 'tailwind',
-    component: TryTailwindComponent,
   },
 ];
